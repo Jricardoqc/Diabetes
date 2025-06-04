f5de3fe434b2deec4e13fa83c163a70e4ac61d87
Joao Ricardo <rjoao148@gmail.com>
2025-06-04 16:09:37 -0300

    mudando o link do fetch

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ function enviarFormulario() {
     blood_glucose_level: Number(document.getElementById("glicose").value)
   };
 
-  fetch("http://127.0.0.1:5000/predict", {
+  fetch("https://diabetes-jp84.onrender.com/HTML/Teste.html", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(dados)
